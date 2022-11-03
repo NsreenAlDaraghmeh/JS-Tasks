145b975a8d0696bdbdc8e8dfa213f6826fa553fd
nsreen <nsreen@mail.com>
2022-11-03 15:16:18 +0200

    adding

---
+++ js-task1.js
@@ -0,0 +1,20 @@
+
+     
+      let mark = 89;
+      if (mark < 0 || mark > 100) {
+        document.write("invalid mark");
+      } else if (mark > 0 && mark<50) {
+        document.write("FAIL");
+      }  else if (mark>=50 && mark<60) {
+        document.write("D");
+      }else if (mark>=60 && mark<70) {
+        document.write("C");
+      }else if (mark>=70 && mark<80) {
+        document.write("B");
+      }else if (mark>=80 && mark<90) {
+        document.write("A");
+      }else document.write("A+")
+      
+    
+ 
+  
---
+++ js-task2.js
@@ -0,0 +1,4 @@
+const pie = 3.14;
+let r = 5 ;
+const circleArea = r**2*pie;
+document.write(circleArea);
