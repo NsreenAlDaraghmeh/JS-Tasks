d5451acd77a2780c115c104029abffd3dfa59e0b
nsreen <nsreen@mail.com>
2022-11-06 16:57:55 +0200

    adding

---
+++ 6-11 js-tasks/task1.js
@@ -0,0 +1,59 @@
+
+let birthdateMonth = window.prompt("write your birthdate month :",1);
+switch (birthdateMonth )
+{
+    case "1":
+        month="jan";
+        document.write("The input:"+ birthdateMonth +"<br>"+"The output:" + month )
+        break;
+        case "2":
+            month="feb";
+            document.write("The input:"+ birthdateMonth +"<br>"+"The output:" + month )
+            break;
+            case "3":
+                month="mar";
+                document.write("The input:"+ birthdateMonth +"<br>"+"The output:" + month )
+                break;
+                case "4":
+                    month="apr";
+                    document.write("The input:"+ birthdateMonth +"<br>"+"The output:" + month )
+                    break;
+                    case "5":
+                        month="may";
+                        document.write("The input:"+ birthdateMonth +"<br>"+"The output:" + month )
+                        break;
+                        case "6":
+                            month="jun";
+                            document.write("The input:"+ birthdateMonth +"<br>"+"The output:" + month )
+                            break;
+                            case "7":
+                                month="july";
+                                document.write("The input:"+ birthdateMonth +"<br>"+"The output:" + month )
+                                break;
+                                case "8":
+                                    month="aug";
+                                    document.write("The input:"+ birthdateMonth +"<br>"+"The output:" + month )
+                                    break; 
+                                    case "9":
+                                        month="sep";
+                                        document.write("The input:"+ birthdateMonth +"<br>"+"The output:" + month )
+                                        break;
+                                        case "10":
+                                            month="oct";
+                                            document.write("The input:"+ birthdateMonth +"<br>"+"The output:" + month )
+                                            break;
+                                            case "11":
+                                                month="nov";
+                                                document.write("The input:"+ birthdateMonth +"<br>"+"The output:" + month )
+                                                break;
+                                                default:
+                                                    month="dec";
+                                                    document.write("The input:"+  birthdateMonth +"<br>"+"The output:" + month )
+}
+
+
+
+
+
+    
+
