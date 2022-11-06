8dbbce9f317b804f2aeccedd939ac3ba3b68ce78
nsreen <nsreen@mail.com>
2022-11-06 17:10:25 +0200

    update

--- 6-11 js-tasks/task-2.js
+++ 6-11 js-tasks/task-2.js
@@ -1,22 +1,22 @@
 
-// let y=1
-// for(let r=1;r<5;r++){
-//     for(let c=1;c<=r;c++){
-//         document.write(y + " ")
-//         y+=1
-//     }
-//     document.write("<br>")
-// }
-
-for (let i=1;i<=10;i++){
-    if(i==1){
-        document.write(i + "<br>")
-    }else if (i==3){
-        document.write(i + "<br>")
-    }else if (i==6){
-        document.write(i + "<br>")
-    }else{
-        document.write(i + " ")
-
+let y=1
+for(let r=1;r<5;r++){
+    for(let c=1;c<=r;c++){
+        document.write(y + " ")
+        y+=1
     }
+    document.write("<br>")
 }
+
+// for (let i=1;i<=10;i++){
+//     if(i==1){
+//         document.write(i + "<br>")
+//     }else if (i==3){
+//         document.write(i + "<br>")
+//     }else if (i==6){
+//         document.write(i + "<br>")
+//     }else{
+//         document.write(i + " ")
+
+//     }
+// }
